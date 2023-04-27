0bf409cadb74ac8f1f33028878e8adb41aecd27e
kinari321 <abcdream321@yahoo.co.jp>
2023-04-27 21:35:50 +0900

    3章の全ての問題の解答を記述

---
+++ 03_io.Reader/3.9-question6.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+var (
+	computer    = strings.NewReader("COMPUTER")
+	system      = strings.NewReader("SYSTEM")
+	programming = strings.NewReader("PROGRAMMING")
+)
+
+/*
+Q3.6:ストリーム総集編
+「ASCII」という文字を出力
+*/
+func main() {
+	var stream io.Reader
+	charA := io.NewSectionReader(programming, 5, 1)
+	charS := io.NewSectionReader(system, 0, 1)
+	charC := io.NewSectionReader(computer, 0, 1)
+	charI1 := io.NewSectionReader(programming, 8, 1)
+	charI2 := io.NewSectionReader(programming, 8, 1)
+	stream = io.MultiReader(charA, charS, charC, charI1, charI2)
+
+	io.Copy(os.Stdout, stream)
+}
